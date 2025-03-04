e9ea7e5b7a7ef514e140806dd93cbc97984b7685
bitard2112 <olegermolaev54@gmail.com>
2025-03-04 21:15:20 +0300

    Added script Health

---
+++ Void Shop/Assets/Scripts/ManagersScripts/Health/PlayerHealth.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class PlayerHealth : Health
+{
+    private Regeneration regeneration;
+
+    protected override void Awake() 
+    {
+        base.Awake();
+        regeneration = gameObject.AddComponent<Regeneration>();
+        regeneration.Initialize(this);
+    }
+
+    public override void TakeDamage(int damage) 
+    {
+        base.TakeDamage(damage);
+        regeneration.TakeDamage();
+    }
+
+    public override void Regeneration(int amount) 
+    {
+        base.Regeneration(amount);
+        if (CurrentHP == MaxHP)
+        {
+            Destroy(regeneration);
+        }
+    }
+}
