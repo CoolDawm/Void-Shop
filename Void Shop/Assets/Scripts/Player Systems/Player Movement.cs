f3fb405876789f67abf3a19c4e06316d311b6210
Gleb <karamvoldemar@gmail.com>
2025-03-05 12:36:30 +0300

    URP Tweaks

---
+++ Void Shop/Assets/Scripts/Player Systems/Player Movement.cs
@@ -0,0 +1,96 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using Cinemachine;
+
+[RequireComponent(typeof(CharacterController), typeof(PlayerInput))]
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float _moveSpeed = 5f;
+    [SerializeField] private float _jumpHeight = 3f;
+    [SerializeField] private float _gravity = -9.81f;
+    [SerializeField] private float _rotationSpeed = 100f;
+    [SerializeField] private CinemachineVirtualCamera _virtualCamera;
+    [SerializeField] private Camera mainCamera; 
+
+    private CharacterController _controller;
+    private PlayerInput _playerInput;
+    private InputAction _moveAction;
+    private InputAction _jumpAction;
+    private InputAction _lookAction;
+
+    private CinemachinePOV _povComponent;
+    private Vector3 _velocity;
+    private float _verticalRotation;
+
+    private void Awake()
+    {
+        _controller = GetComponent<CharacterController>();
+        _playerInput = GetComponent<PlayerInput>();
+
+        _moveAction = _playerInput.actions["Move"];
+        _jumpAction = _playerInput.actions["Jump"];
+        _lookAction = _playerInput.actions["Look"];
+        _virtualCamera = FindAnyObjectByType<CinemachineVirtualCamera>();
+        _povComponent = _virtualCamera.GetCinemachineComponent<CinemachinePOV>();
+
+        if (mainCamera == null)
+        {
+            mainCamera = Camera.main;
+        }
+
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    private void Update()
+    {
+        HandleMovement();
+        HandleLook();
+        HandleJump();
+        ApplyGravity();
+    }
+
+    private void HandleMovement()
+    {
+        Vector2 input = _moveAction.ReadValue<Vector2>();
+
+        Vector3 cameraForward = mainCamera.transform.forward;
+        Vector3 cameraRight = mainCamera.transform.right;
+
+        cameraForward.y = 0;
+        cameraRight.y = 0;
+        cameraForward.Normalize();
+        cameraRight.Normalize();
+
+        Vector3 move = (cameraForward * input.y + cameraRight * input.x).normalized;
+        _controller.Move(move * _moveSpeed * Time.deltaTime);
+    }
+
+    private void HandleLook()
+    {
+        Vector2 look = _lookAction.ReadValue<Vector2>() * _rotationSpeed * Time.deltaTime;
+        transform.Rotate(Vector3.up * look.x);
+
+        _verticalRotation -= look.y;
+        _verticalRotation = Mathf.Clamp(_verticalRotation, -90f, 90f);
+        _povComponent.m_VerticalAxis.Value = _verticalRotation;
+    }
+
+    private void HandleJump()
+    {
+        if (_jumpAction.triggered && _controller.isGrounded)
+        {
+            _velocity.y = Mathf.Sqrt(_jumpHeight * -2f * _gravity);
+        }
+    }
+
+    private void ApplyGravity()
+    {
+        if (_controller.isGrounded && _velocity.y < 0)
+        {
+            _velocity.y = -2f;
+        }
+
+        _velocity.y += _gravity * Time.deltaTime;
+        _controller.Move(_velocity * Time.deltaTime);
+    }
+}
